8876935aceeaf70a1130825f616fbe557c5f4a08
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-10-13 23:18:33 +0600

    Updates

---
+++ precache-manifest.60035f0e12821de70e5990008bbd0776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17883abb3a1e03bf805b09c7e49909e",
+    "url": "/energypac_CRM/index.html"
+  },
+  {
+    "revision": "8be154200e7453a54d48",
+    "url": "/energypac_CRM/static/css/2.1a1b9e1e.chunk.css"
+  },
+  {
+    "revision": "3e903a188a5c8a1d596e",
+    "url": "/energypac_CRM/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8be154200e7453a54d48",
+    "url": "/energypac_CRM/static/js/2.153c4db0.chunk.js"
+  },
+  {
+    "revision": "3e903a188a5c8a1d596e",
+    "url": "/energypac_CRM/static/js/main.286a6c64.chunk.js"
+  },
+  {
+    "revision": "c340bcadf6f1ed52a242",
+    "url": "/energypac_CRM/static/js/runtime-main.90edd169.js"
+  }
+]);
